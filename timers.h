78d3b6707f6b3fbc4961c557e450a8b3b1ad0f06
Mihir Singh Yadav <46639161+MihirSinghYadav@users.noreply.github.com>
2024-04-12 12:21:37 +0530

    timers.h

---
+++ timers.h
@@ -0,0 +1,11 @@
+// timers.h
+#ifndef TIMERS_H  // Prevents the header file from being included multiple times.
+#define	TIMERS_H 
+
+// Function Prototypes
+void init_timer0(void);  // Function to initialize Timer 0
+void init_timer2(void);  // Function to initialize Timer 2
+
+#endif	/* TIMERS_H */
+
+
